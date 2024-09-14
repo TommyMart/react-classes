832c9a17614fe1371970c9d2218c59f7f2e2b7d8
Thomas Martin <161907136+TommyMart@users.noreply.github.com>
2024-09-14 10:26:07 +0930

    feat: react class components

---
+++ reactjs-pokemon-class-demo/src/PokemonFetcher.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PokemonCard from './PokemonCard';
+
+
+// const pokemonList = ['pikachu', 'squirtle', 'mewtwo'];
+export default class PokemonFetcher extends React.Component {
+
+    constructor(props){
+        // super is React.Component
+        super(props);
+        // change content on screen without refreshing 
+        // only component refreshes not whole page
+        this.state = {
+            pokemonList: ['pikachu', 'squirtle', 'mewtwo']
+        }
+    }
+
+
+    render(){
+        return (
+            <div>
+                <h1>Pokemon Data</h1>
+                {this.state.pokemonList.map(pokemon => {
+                return <PokemonCard name={pokemon}/>
+                })}
+                <button onClick={() => {
+                    this.setState({pokemonList: []});
+                }}>
+                    Empty the state
+                </button>
+            </div>
+        );
+    } 
+}
